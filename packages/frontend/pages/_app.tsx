1b211d87c7dc928a3c91664e142a3cb650b5caf7
kei32bit <keinishino78@gmail.com>
2021-09-28 00:47:36 +0900

    add header

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,8 +1,27 @@
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import Link from "next/link";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MarketPlace({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">NFT Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-green-400">Home</a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-green-400">Create NFT</a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-green-400">My NFTs</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp;
+export default MarketPlace;
