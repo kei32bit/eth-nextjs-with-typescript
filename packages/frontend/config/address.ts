6339f337481eddc49a77898b5d1515c2b0b0af87
kei32bit <keinishino78@gmail.com>
2021-09-30 03:42:58 +0900

    add contract address

---
+++ packages/frontend/config/address.ts
@@ -0,0 +1,7 @@
+// Rinlerby
+export const nftAddress = "0xBb5575AEcf4F3E8D85D2014762bD2858c457581d";
+export const nftMarketAddress = "0xf1Ad1DfB1524fBD1Dd9845d4f399C7C093F56bb0";
+
+// Matic
+// export const nftAddress = "0x2d6f629A2F75A83D3F08CD40F3881FA334113F11";
+// export const nftMarketAddress = "0x2cDd653E8Bc4cE0bBA06261f573fd5b2858B6D2b";
